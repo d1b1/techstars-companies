ae66a12eb16a0e8d7a41130f12bc2d6f87668437
Stephan L. Smith <stephan@meenta.io>
2024-04-25 15:57:35 -0400

    Added more formatting and copy

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import crunchbaseLogo from './assets/crunchbase.png';
 import linkedInLogo from './assets/linkedIn.png';
 import twitterLogo from './assets/twitter.png';
 import GitHubButton from 'react-github-btn';
-import CustomModal from './Modal2';
+import CustomModal from './Modal_ValueProp';
 
 import {
   Configure,
@@ -17,13 +17,12 @@ import {
   SearchBox,
   RefinementList,
   HitsPerPage,
-  Stats
+  Stats,
+  SortBy
 } from 'react-instantsearch';
 
 import { Panel } from './Panel';
-
 import type { Hit } from 'instantsearch.js';
-
 import './App.css';
 
 const searchClient = algoliasearch(
@@ -52,99 +51,130 @@ const future = { preserveSharedStateOnUnmount: true };
 export function App() {
   const [isModalOpen, setIsModalOpen] = useState(false);
 
-  const handleOpenModal = () => {
+  const openModal = () => {
     setIsModalOpen(true);
   };
+
+  const closeTheModal = () => {
+    localStorage.setItem('alreadySeenModal', 'yes');
+    setIsModalOpen(false);
+  };
+
+  try {
+    setTimeout(() => {
+      const alreadySeenModal = localStorage.getItem('alreadySeenModal') || 'no';
+      if (alreadySeenModal === 'no') {
+        setIsModalOpen(true);
+      }
+    }, 1000);
+  } catch(err) {
+    console.log('Small error')
+  }
 
   return (
     <div>
 
-        <InstantSearch
-          searchClient={searchClient}
-          indexName="Techstars-Companies"
-          future={future}
-          routing={true}
-        >
+      <InstantSearch
+        searchClient={searchClient}
+        indexName="Techstars-Companies"
+        future={future}
+        routing={true}
+      >
 
-      <header className="header">
-        <h1 className="header-title">
-          Techstars Companies (v2) 
-          <Stats />
-        </h1>
-        <div className="gh-btn">
-          
-          <GitHubButton href="https://github.com/d1b1/techstars-companies" data-color-scheme="no-preference: light; light: light; dark: dark;" data-size="large" data-show-count="true" aria-label="Star d1b1/techstar-companies on GitHub">Star</GitHubButton>
-        </div>
-      </header>
+        <header className="header">
+          <h1 className="header-title">
+            Techstars Companies (v2)
+            <Stats />
+          </h1>
+          <button className="btn btn-sm btn-outline-dark avatar-btn" onClick={openModal}>
+            <img src="https://stephansmith.solutions/assets/clients/stephan-smith-avatar.png" className="avatar_img" />
+            Startup Tools
+          </button>
+        </header>
 
-      <div className="container-fluid">
+        <div className="container-fluid">
 
-        <CustomModal
-          isOpen={isModalOpen}
-          onRequestClose={() => setIsModalOpen(false)}
-          content={<p>Modal Content</p>}
-        />
-        
-        <Configure hitsPerPage={25} />
+          <CustomModal
+            isOpen={isModalOpen}
+            onRequestClose={() => closeTheModal()}
+            content={
+              <p>Modal Content</p>
+            }
+          />
+
+          <Configure hitsPerPage={25} />
 
           <div className="row">
             <div className="col-3 d-none d-md-block d-lg-block">
 
-            <div className="filter-el">
+              <div className="filter-el">
                 <h4>
-                  Industry:
+                  Sort By:
+                </h4>
+                <SortBy
+                  ///defaultRefinement="Techstars-Companies"
+                  items={[
+                    { value: 'Techstars-Companies', label: 'Youngest to Oldest' },
+                    { value: 'Techstars-Companies_by_year_asc', label: 'Oldest to Youngest' }
+                  ]}
+                />
+              </div>
+
+              <div className="filter-el">
+                <h4>
+                  What vertical or focus?:
                 </h4>
                 <RefinementList searchable="true" searchablePlaceholder="Enter a vertical..." attribute="industry_vertical" />
               </div>
 
               <div className="filter-el">
                 <h4>
-                  Industry:
+                  What Techstars program?
                 </h4>
-                <RefinementList searchable="true" searchablePlaceholder="Enter a vertical..." attribute="program_names" limit="5" />
+                <RefinementList searchable="true" searchablePlaceholder="Enter a program name..." attribute="program_names" limit="5" />
               </div>
 
               <div className="filter-el">
                 <h4>
-                  Program Year:
+                  Which program year?
                 </h4>
                 <RefinementList attribute="first_session_year" />
               </div>
 
               <div className="filter-el">
                 <h4>
-                  Program City:
+                  Which program city?
                 </h4>
                 <RefinementList searchable="true" attribute="city" searchablePlaceholder="Enter city..." limit="5" />
               </div>
 
               <div className="filter-el">
                 <h4>
-                  State/Province:
+                  Which State?
                 </h4>
                 <RefinementList attribute="state_province" />
               </div>
 
               <div className="filter-el">
                 <h4>
-                  Country:
+                  Which Company?
                 </h4>
                 <RefinementList attribute="country" />
               </div>
-              
+
             </div>
             <div className="col-md-9 p-4">
 
               <SearchBox placeholder="Enter a company name..." className="searchbox" />
 
-              <Hits hitComponent={Hit}/>
+              <Hits hitComponent={Hit} />
 
-              <br/>
-              <Pagination padding={2}/>
- 
+              <br />
+              <Pagination padding={2} />
+
             </div>
+          </div>
         </div>
-      </div>
       </InstantSearch>
     </div>
   );
@@ -194,13 +224,13 @@ function Hit({ hit }: HitProps) {
           <p>
             <Highlight attribute="brief_description" hit={hit} />
             &nbsp;
-            { hit['website'] && <a href={`https://${hit['website']}`} target="_blank">https://{hit['website']}</a>}
+            {hit['website'] && <a href={`https://${hit['website']}`} target="_blank">https://{hit['website']}</a>}
           </p>
 
           <p>
             <b>HQ Location:</b> {hit.state_province}, {hit.city},&nbsp;
             <b>Program:</b> {hit['program_names']}&nbsp;
-  
+
             <div className="m-2">
               {(hit.industry_vertical || []).map((item, index) => (
                 <span key={index} className="badge bg-secondary me-1">
@@ -217,22 +247,25 @@ function Hit({ hit }: HitProps) {
         </div>
         <div className="col-2 text-end">
 
-            {hit['crunchbase_url'] ?
-              <a href={`https://${hit['crunchbase_url']}`} target="_blank">
-                <img src={crunchbaseLogo} className="crunch" />
-              </a>
-              : null}
-            {hit['linkedin_url'] ?
-              <a href={`https://${hit['linkedin_url']}`} target="_blank">
-                <img src={linkedInLogo} width="40" className="crunch" />
-              </a>
-              : null}
-            {hit['twitter_url'] ?
-              <a href={`https://${hit['twitter_url']}`} target="_blank">
-                <img src={twitterLogo} className="crunch" width="40" />
-              </a>
-              : null}
-              
+          {hit['crunchbase_url'] ?
+            <a href={`https://${hit['crunchbase_url']}`} target="_blank">
+              {/* <img src={crunchbaseLogo} className="crunch" /> */}
+              <svg fill="#000000" viewBox="0 0 24 24" className="crunch" role="img" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><title>Crunchbase icon</title><path d="M21.6 0H2.4A2.41 2.41 0 0 0 0 2.4v19.2A2.41 2.41 0 0 0 2.4 24h19.2a2.41 2.41 0 0 0 2.4-2.4V2.4A2.41 2.41 0 0 0 21.6 0zM7.045 14.465A2.11 2.11 0 0 0 9.84 13.42h1.66a3.69 3.69 0 1 1 0-1.75H9.84a2.11 2.11 0 1 0-2.795 2.795zm11.345.845a3.55 3.55 0 0 1-1.06.63 3.68 3.68 0 0 1-3.39-.38v.38h-1.51V5.37h1.5v4.11a3.74 3.74 0 0 1 1.8-.63H16a3.67 3.67 0 0 1 2.39 6.46zm-.223-2.766a2.104 2.104 0 1 1-4.207 0 2.104 2.104 0 0 1 4.207 0z"></path></g></svg>
+            </a>
+            : null}<br/>
+          {hit['linkedin_url'] ?
+            <a href={`https://${hit['linkedin_url']}`} target="_blank">
+              {/* <img src={linkedInLogo} width="40" className="crunch" /> */}
+              <svg xmlns="http://www.w3.org/2000/svg" className="crunch" viewBox="0 0 448 512"><path d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z"/></svg>
+            </a>
+            : null}<br/>
+          {hit['twitter_url'] ?
+            <a href={`https://${hit['twitter_url']}`} target="_blank">
+              {/* <img src={twitterLogo} className="crunch" width="40" /> */}
+              <svg xmlns="http://www.w3.org/2000/svg" className="crunch" viewBox="0 0 448 512"><path d="M64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H384c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H64zm297.1 84L257.3 234.6 379.4 396H283.8L209 298.1 123.3 396H75.8l111-126.9L69.7 116h98l67.7 89.5L313.6 116h47.5zM323.3 367.6L153.4 142.9H125.1L296.9 367.6h26.3z"/></svg>
+            </a>
+            : null}
+
         </div>
       </div>
     </article>
---
+++ src/Modal_ValueProp.tsx
@@ -0,0 +1,87 @@
+// CustomModal.js
+import React from 'react';
+import Modal from 'react-modal';
+
+const CustomModal = ({ isOpen, onRequestClose, content }) => (
+  <Modal isOpen={isOpen} onRequestClose={onRequestClose}>
+    <button className="btn btn-success" onClick={onRequestClose}>
+      Find Some Startups &rarr;
+    </button>
+    <div className="row mt-3">
+      <div className="offset-0 col-90">
+        <h2>
+          Why make Techstars Companies Searchable?
+        </h2>
+        <br />
+        <p>
+          Data is valuable in itself! Startups need to be intentional about
+          how they use their time. This dataset is a good starting
+          point for a number of startup related activities; <b>finding investors,
+          finding early customers, and learning how to find customers.</b>
+        </p>
+        <hr className="mt-5" />
+        <h3>
+          Use Case 1: Investors of Look-A-like Companies
+        </h3>
+        <p>
+          If you are a founder, looking for investors, a good starting point is
+          to find other companies that are similar, and ahead of you. Meaning they
+          have a product in the marketplace, some form of revenue, and have raised
+          venture funding. (Their early investors can be good fit you, now.)
+        </p>
+        <div className="alertBox">
+          <div className="row">
+            <div className="col-md-6">
+              <p>
+                Use your network to get introductions to their founders, ask for
+                help, advise and warm intros to their investors. Startup founders know 
+                the pain of finding investors, so are very willing to help. They 
+                can walk you in the door and get you visibility that would take 
+                a lot longer on your own.
+              </p>
+            </div>
+            <div className="col-md-6">
+              <ol>
+                <li>Filter by vertical</li>
+                <li>Filter by age.</li>
+                <li>Search funding status on Crunchbase.</li>
+                <li>Look for a pattern and then network your way in.</li>
+                <li>Build a target list or ICP.</li>
+                <li>Start Networking with intention.</li>
+              </ol>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-12">
+              <b>Pro Tip:</b> The Managing Director (MD) of Techstars program can be very helpful.
+              MDs are super connectors, and know everyone in their program ecosystem. You, as a 
+              new startup founder are their future dealflow. Reach out.
+            </div>
+          </div>
+        </div>
+        <hr className="mt-5" />
+        <h3>
+          Use Case 2: Finding Early Adaptors
+        </h3>
+        <p>
+          If you are testing out a new product or service, startups can be a good
+          place to find early adaptors. Startup founders are always looking for
+          an advantage. They are <b>time</b>, <b>funding</b> and <b>staff</b> restricted. Early adaptors
+          are great beta clients. They give a lot of value, can be bug and UI tolerate,
+          and are grateful for perks!
+        </p>
+        <div className="alertBox">
+          <p>
+            Startups love free! And know how to help.
+          </p>
+        </div>
+        <br />
+        <p>
+          Good luck and good hunting. :)
+        </p>
+      </div>
+    </div>
+  </Modal>
+);
+
+export default CustomModal;
